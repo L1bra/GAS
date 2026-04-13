09d28e992615320cf3f1a4fb17793e60db5ef209
L1bra <L1bradv86@gmail.com>
2026-04-14 02:04:16 +1000

    Custom Ability Tasks

---
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -0,0 +1,28 @@
+// Copyright Crutch Inc.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(DisplayName = "TargetDataUnderMouse", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true"))
+	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataSignature ValidData;
+private:
+	void Activate() override;
+};
