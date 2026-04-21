5ff236b3c93ab0957b608f18e14a0a446cbca701
L1bra <L1bradv86@gmail.com>
2026-04-21 15:43:00 +1000

    Projectile impact

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -7,6 +7,8 @@
 #include "AuraProjectile.generated.h"
 
 class USphereComponent;
+class UNiagaraSystem;
+class UAudioComponent;
 class UProjectileMovementComponent;
 
 UCLASS()
@@ -22,11 +24,29 @@ public:
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
 
 protected:
-	virtual void BeginPlay() override;
+	void BeginPlay() override;
+	void Destroyed() override;
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> ImpactSound;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> LoopingSound;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UAudioComponent> LoopingSoundComponent;
+
+	bool bHit = false;
+
+	UPROPERTY(EditDefaultsOnly)
+	float LifeSpawn = 15.f;
 };
